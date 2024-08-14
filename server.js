716047a4637b68c6aa64f29d7197ca53d40c528e
E-shopee Card E-commerce base website <https://github.com/RIYAZBAIG>
2024-08-14 18:19:57 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+
+const notes = require("./notes.js")
+var _ = require('lodash');
+
+console.log('server file is available....!');
+
+var age = notes.age;
+console.log(age);
+
+var data = ["person","person",1,2,1,2,'name','age','2'];
+var filter = _.uniq(data);
+console.log(filter);
+console.log(_.isString("samadbaig"));
+
+
+var arr = [1,2,3];
+
+_.reverse(arr);
+console.log(arr)
+
+// console.log("server file is running on 3000");
+
+
+
+// function add(a,b) {
+//     return a+b;
+    
+// }
+
+// var result = add(51,15);
+// console.log(result);
+
+
+// function callback() {
+//     console.log("Riyaz is calling callback function");
+    
+// }
+
+
+// const add = function (a,b, callback) {
+//     var result = a+b;
+//     console.log('result: '+ result);
+//     callback();
+    
+// }
+
+// add(45,5,callback);
+
+
+// var fs = require('fs');
+// var os = require('os');
+// var user = os.userInfo();
+// console.log(user.username);
+
+// fs.appendFile("greeting.txt", 'Hi '+ user.username + '!\n', ()=>{
+//     console.log('file is created....!');
+// });
+
+// console.log(os);
+// console.log(fs);
